847aff259a89bffe73939f15ac18e6862330f706
Ikko-T <skyblueboy32@gmail.com>
2021-08-11 15:05:05 +0900

    Added jQuery functions.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
-$(document).ready(function () {
+$(function () {
   // スライドショー
-  $('.slider').bxSlider({
-    auto: true,
-    mode: 'fade',
-    speed: 1000,
-    pause: 3000,
-    pager: false,
-    controls: false,
+  $(document).ready(function () {
+    $('.slider').bxSlider({
+      auto: true,
+      mode: 'fade',
+      speed: 1000,
+      pause: 4000,
+      pager: false,
+      controls: false,
+    })
+  })
+
+  // 画像フェードイン
+  $(window).scroll(function () {
+    $('.scroll-fade').each(function () {
+      var targetElement = $(this).offset().top
+      var scroll = $(window).scrollTop()
+      var windowHeight = $(window).height()
+      if (scroll > targetElement - windowHeight + 200) {
+        $(this).css('opacity', '1')
+        $(this).css('transform', 'translateY(0)')
+      }
+    })
   })
 })
