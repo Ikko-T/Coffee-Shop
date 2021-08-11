4a0a594998110c3776ce90f63df5d025438a6a8e
Ikko-T <skyblueboy32@gmail.com>
2021-08-11 13:22:15 +0900

    Applied responsive design.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // スライドショー
+  $('.slider').bxSlider({
+    auto: true,
+    mode: 'fade',
+    speed: 1000,
+    pause: 3000,
+    pager: false,
+    controls: false,
+  })
+})
